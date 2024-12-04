5efb8d0e3421c49e798dc73984c2be13d44cd080
k3vinphuong <161548597+k3vinphuong@users.noreply.github.com>
2024-12-04 17:36:05 +1100

    cleaned up the navbar and footer

--- pawsitivedaycare/src/App.js
+++ pawsitivedaycare/src/App.js
@@ -7,9 +7,9 @@ import "./App.css";
 
 function App() {
     return (
-        <div className="App">
+        <div className="page-container">
             <Navbar />
-            <main>
+            <main className="content">
                 <Hero />
                 <Services />   
             </main>
--- pawsitivedaycare/src/components/Navbar.js
+++ pawsitivedaycare/src/components/Navbar.js
@@ -14,21 +14,28 @@ function Navbar() {
         <header className="navbar">
             <a href="#home" className="logo">
                 <img src={logo} alt="PAWsitive Daycare Logo"
-                style={{height: "40px", marginRight: "10px"}}/>PAWsitive Daycare
+                style={{height: "40px", marginRight: "10px"}}/>
+                <span className="brand-name">
+                    <strong>PAWsitive</strong>
+                    <br />
+                    Daycare
+                </span>
             </a>
             
-            <nav className={`nav-links ${menuOpen ? "mobile" : ""}`}>
-                <ul>
+            <nav className={`nav-content ${menuOpen ? "mobile" : ""}`}>
+                <ul className="nav-links">
                     <li><a href="#home">Home</a></li>
                     <li><a href="#services">Services</a></li>
                     <li><a href="#pricing">Pricing</a></li>
                     <li><a href="#contact">Contact Us</a></li>
                 </ul>
-                <div className="nav-button">
                     <button className="login-btn">
                         <span className="login-text">Log In</span>
                     </button>
-                </div>
+                    <button className="signup-btn">
+                        <span className="signup-text">Sign Up</span>
+                    </button>
+                    
             </nav>
             <button className="hamburger-menu" onClick={toggleMenu}>
                 {menuOpen ? "✖" : "☰"}
