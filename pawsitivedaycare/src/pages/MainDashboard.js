93e437b07f2e2789b3816f1f24ab3002ac6dfe89
k3vinphuong <161548597+k3vinphuong@users.noreply.github.com>
2024-12-11 16:12:51 +1100

    fixed the layout of the services page and added icons

--- pawsitivedaycare/src/pages/MainDashboard.js
+++ pawsitivedaycare/src/pages/MainDashboard.js
@@ -9,21 +9,10 @@ function Dashboard() {
         <h1>Dashboard</h1>
       </header>
 
-      {/* Account and Pets Section */}
-      <div className="dashboard-actions">
-        <div className="action-card">
-          <h2>My Account</h2>
-          <button className="action-button">Update my details</button>
-        </div>
-        <div className="action-card">
-          <h2>My Pets</h2>
-          <button className="action-button">Update my pets</button>
-        </div>
-      </div>
 
       {/* Bookings Section */}
       <div className="dashboard-bookings">
-        <h2>My bookings</h2>
+        <h2>My Bookings</h2>
         <div className="booking-cards">
           <div className="booking-card">
             <h3>Booking 1</h3>
--- pawsitivedaycare/src/pages/ServicesPage.js
+++ pawsitivedaycare/src/pages/ServicesPage.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/ServicesPage.css";
 import Rabbit from "../assets/rabbit.jpg";
 import Parrot from "../assets/parrot.jpg";
 import Hamster from "../assets/Hamster.jpg";
 import { Link } from "react-router-dom";
 
-function ServicesPage() {
+const ServicesPage = ({ icon, title, description }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div className="services-page">
       {/* Hero Section */}
@@ -19,44 +21,34 @@ function ServicesPage() {
       <div className="services-details">
         <div className="services-section">
           <h2>Pet Daycare</h2>
-          <div className="services-content">
-            <p className="services-description">
-              Keep your pet happy and healthy with play dates, exercise, and playful games, all supervised by our expert PAWsitive team!
-            </p>
-            <div className="services-pricing">
-              <h3>Prices</h3>
-            </div>
-            
+          <div className="services-content"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}>
             <img src={Rabbit} alt="Small Animal" className="service-image" />
+            <h3 className="services-pricing">Prices</h3>
+            {isHovered && <p className="services-description">Keep your pet happy and healthy with play dates, exercise, and playful games, all supervised by our expert PAWsitive team</p>}
           </div>
         </div>
 
-        <div className="service-section">
+        <div className="services-section">
           <h2>Pet Grooming</h2>
-          <div className="services-content">
-            <p className="services-description">
-              We also offer grooming services so your pets can enjoy a bath, clip, and tidy, leaving them clean and looking their best!
-            </p>
-            <div className="services-pricing">
-            <h3>Prices</h3>
-            </div>
-            
+          <div className="services-content"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}>
             <img src={Parrot} alt="Small Animal" className="service-image" />
+            <h3 className="services-pricing">Prices</h3>
+            {isHovered && <p className="services-description">We also offer grooming services so your pets can enjoy a bath, clip, and tidy, leaving them clean and looking their best!</p>}
           </div>
         </div>
-
-        <div className="service-section">
+        
+        <div className="services-section">
           <h2>Dog Walking</h2>
-          <div className="services-content">
-            <p className="services-description">
-              We provide professional dog walking services to keep your furry friend active, happy, and well-exercised, no matter how busy your day gets!
-            </p>
-            <div className="services-pricing">
-              <h3>Prices</h3>
-              
-            </div>
-            
+          <div className="services-content"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}>
             <img src={Hamster} alt="Small Animal" className="service-image" />
+            <h3 className="services-pricing">Prices</h3>
+            {isHovered && <p className="services-description">We provide professional dog walking services to keep your furry friend active, happy, and well-exercised, no matter how busy your day gets!</p>}
           </div>
         </div>
       </div>
