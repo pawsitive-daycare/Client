df0c92513cd022b9be858ff062d7371ef245a8a9
John Niduelan <jeniduelan@gmail.com>
2024-12-18 01:19:35 +1100

    loginuser imported

--- pawsitivedaycare/src/pages/LoginPage.js
+++ pawsitivedaycare/src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "../styles/LogInPage.css";
+import { loginUser } from "../components/api";
 
 const Login = () => {
   const [loginFormData, setLoginFormData] = useState({
